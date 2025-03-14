6deef7bb955fc7892c1c7901ebde7ffc397a6122
Richard-250 <cyubahirorichard250@gmail.com>
2025-03-14 22:46:24 +0200

    making example class

--- example.js
+++ example.js
@@ -1,11 +1,68 @@
 const fruits = ["apple", "banana", "orange"];
 
-// Access elements by index
-console.log(fruits[0]); // Output: "apple"
+console.log(fruits[0]); 
 
-// Add an element
 fruits.push("mango");
-console.log(fruits); // Output: ["apple", "banana", "orange", "mango"]
+console.log(fruits); 
 
-// Check length
-console.log(fruits.length); // Output: 4
+console.log(fruits.length); 
+
+
+
+function Queue () {
+    collection = [];
+
+    this.print = function() {
+        console.log(collection);
+    };
+     
+    this.enqueue = function(element) {
+        collection.push(element);
+    };
+
+    this.dequeue = function() {
+        return collection.shift();
+    };
+
+    this.front = function() {
+        return collection[0];
+    };
+
+    this.size = function() {
+        return collection.length;
+    };
+
+    this.empty = function() {
+        return (collection.length === 0);
+    };
+}
+
+
+
+
+
+class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+
+    get peek() {
+        return this.items[this.top];
+    }
+
+    push(value) {
+        this.top += 1;
+        this.items[this.top] = value
+    }
+}
+
+const stack = new Stack;
+console.log("stack:", stack.top)
+console.log("items:", stack.items)
+stack.push("examplesssss");
+stack.push("come on come on");
+console.log(stack.items)
+console.log(stack.peek)
+console.log(stack.top);
+console.log(stack.items[1]);
