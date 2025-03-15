e4638c349e0283ac5572cd4e4afdd0d5f06b793f
Richard-250 <cyubahirorichard250@gmail.com>
2025-03-15 22:32:24 +0200

    example promises

--- example.js
+++ example.js
@@ -66,3 +66,27 @@ console.log(stack.items)
 console.log(stack.peek)
 console.log(stack.top);
 console.log(stack.items[1]);
+
+
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
+const numbers = ['1', '2', '3', '4', '5', '6'];
+
+const [a, c, ...rest] = alphabet;
+const [b, g, d] = numbers;
+console.log(b, g, d)
+
+const personTwo = {
+    name: "Kajuga",
+    age: 24,
+    Address: {
+        city: "kabuga",
+        village: "urumuri",
+    },
+}
+
+function printUser({ name: karm, age, Address: { city, village } }) {
+    console.log(`Name is: ${karm}. Age is: ${age}`);
+    console.log(city, village)
+};  
+
+printUser(personTwo)
